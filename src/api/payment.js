5780c1d79de99c6e492805dbfe395a27112caa87
ssols2 <baseop0905@gmail.com>
2026-04-06 18:47:52 +0900

    [프론트] 260406 Mypage.vue  결제 수단 등록 로직 추가 api/payment.js 결제 수단 등록 관련 api js 추가

---
+++ src/api/payment.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const url = 'http://localhost:8000/api/payment-methods'
+
+// 1. 결제 수단 등록 (Create)
+export const addPaymentReq = (data) => {
+  return axios.post(url, data)
+}
+
+// 2. 결제 수단 목록 조회 (Read)
+export const getPaymentReq = (memId) => {
+  return axios.get(`${url}/${memId}`)
+}
+
+// 3. 대표 결제 수단 설정 (Update)
+export const setDefaultPaymentReq = (methodId) => {
+  return axios.put(`${url}/${methodId}`, { is_default: true })
+}
+
+// 4. 결제 수단 삭제 (Delete)
+export const delPaymentReq = (methodId) => {
+  return axios.delete(`${url}/${methodId}`)
+}
